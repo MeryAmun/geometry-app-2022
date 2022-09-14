87962af9d02de80e98aeb699f4b640747f780b37
MeryAmun <chebesiawah@gmail.com>
2022-09-14 18:34:10 +0100

    Square components and its functions done

--- src/components/GeometricFigures/Rectangle/Rectangle.jsx
+++ src/components/GeometricFigures/Rectangle/Rectangle.jsx
@@ -3,15 +3,14 @@ import "./rectangle.css";
 const data = {
   length: 0,
   width: 0,
-  unit:'',
+  unit: "",
   dimension: "",
 };
 const Rectangle = () => {
   const [exercise, setExercise] = useState(data);
   const [answer, setAnswer] = useState(0);
-  const [error, setError] = useState('')
-  const [unit, setUnit] = useState('')
-
+  const [error, setError] = useState("");
+  const [unit, setUnit] = useState("");
 
   const onChangeHandler = (e) => {
     const { name, value } = e.target;
@@ -23,65 +22,59 @@ const Rectangle = () => {
 
   const submitHandler = (e) => {
     e.preventDefault();
-    if(exercise.dimension === ''){
-      setError('Please choose the dimension you want to calculate')
+    if (exercise.dimension === "") {
+      setError("Please choose the dimension you want to calculate");
     }
     e.preventDefault();
-    if(exercise.unit === ''){
-      setError('Please select the unit for calculation')
+    if (exercise.unit === "") {
+      setError("Please select the unit for calculation");
+    } else {
+      console.log(exercise);
+      if (exercise.dimension === "Perimeter") {
+        setAnswer((Number(exercise.length) + Number(exercise.width)) * 2);
+      } else if (exercise.dimension === "Area") {
+        setAnswer(
+          Number(exercise.length) * Number(exercise.width),
+          exercise.unit
+        );
+        setUnit(exercise.unit);
+      }
     }
-    else{
-    console.log(exercise);
-    if (exercise.dimension === "Perimeter") {
-      setAnswer((Number(exercise.length) + Number(exercise.width)) * 2);
-    } else if (exercise.dimension === "Area") {
-      setAnswer((Number(exercise.length)) *  Number(exercise.width),exercise.unit);
-      setUnit(exercise.unit)
-    }
-  }
-    setExercise({
-      length:0,
-     width:0,
-     dimension:'',
-    unit:''
-    });
-
-   
+    e.target.reset();
   };
+
   return (
     <div className="app__figure_container">
       <div className="app__figure__header">
-        <h3>Find the Area and Perimeter of A rectangle</h3>
+        <h3>Find the Area and Perimeter of A Rectangle</h3>
       </div>
       <div className="app__form">
         <form onSubmit={submitHandler}>
           <div className="form-group ">
-          <div className="sub_form-group">
-            <input
-              type="radio"
-              id="area"
-              name="dimension"
-              value="Area"
-              onChange={onChangeHandler}
-
-            />
-            <label htmlFor="area">Area</label>
-          </div>
-          <span className="error">{error}</span>
+            <div className="sub_form-group">
+              <input
+                type="radio"
+                id="area"
+                name="dimension"
+                value="Area"
+                onChange={onChangeHandler}
+              />
+              <label htmlFor="area">Area</label>
+            </div>
+            <span className="error">{error}</span>
           </div>
           <div className="form-group ">
-          <div className="sub_form-group">
-            <input
-              type="radio"
-              id="area"
-              name="dimension"
-              value="Perimeter"
-              onChange={onChangeHandler}
-
-            />
-            <label htmlFor="perimeter">Perimeter</label>
-          </div>
-          <span className="error">{error}</span>
+            <div className="sub_form-group">
+              <input
+                type="radio"
+                id="area"
+                name="dimension"
+                value="Perimeter"
+                onChange={onChangeHandler}
+              />
+              <label htmlFor="perimeter">Perimeter</label>
+            </div>
+            <span className="error">{error}</span>
           </div>
           <div className="form-group">
             <input
@@ -90,7 +83,7 @@ const Rectangle = () => {
               onChange={onChangeHandler}
               placeholder="Enter Length"
             />
-              <span className="error">{error}</span>
+            <span className="error">{error}</span>
           </div>
           <div className="form-group">
             <input
@@ -103,25 +96,53 @@ const Rectangle = () => {
           </div>
           <div className="form-group">
             <select name="unit" onChange={onChangeHandler}>
-            <option>Select unit (e.g M for Meters or CM for centimeters)</option>
-            <option value="Meters">Meters</option>
-            <option value="Centimeters">Centimeters</option>
-          </select>
-          <span className="error">{error}</span>
+              <option>
+                Select unit (e.g M for Meters or CM for Centimeters)
+              </option>
+              <option value="M">M</option>
+              <option value="CM">CM</option>
+            </select>
+            <span className="error">{error}</span>
           </div>
           <div className="form-group">
             <button className="btn__custom" type="submit">
               {exercise.dimension === "Perimeter"
                 ? " Calculate " + exercise.dimension
-                :exercise.dimension === "Area" ? " Calculate " + exercise.dimension : 'Calculate'}
+                : exercise.dimension === "Area"
+                ? " Calculate " + exercise.dimension
+                : "Calculate"}
             </button>
           </div>
         </form>
         <div className="app__result_container">
-          <span className="geometric_result">Answer {exercise.unit} in  {unit}: {answer} {unit}</span>
-          <span className="geometric_result">Answer in  {exercise.unit === 'Meters'? 'Centimeters' || exercise.unit === 'Centimeters' : 'Meters' }: {
-            exercise.unit === 'Centimeters' ? answer / 100 || exercise.unit === 'Meters' : answer * 100 
-          }</span>
+          {exercise.dimension === "Area" ? (
+            <span className="geometric_result">
+              Answer in {exercise.unit} : {answer} {exercise.unit}
+              <sup>2</sup>
+            </span>
+          ) : (
+            <span className="geometric_result">
+              Answer in {exercise.unit}: {answer} {exercise.unit}
+            </span>
+          )}
+          {exercise.dimension === "Area" ? (
+            <span className="geometric_result">
+              Answer in{" "}
+              {exercise.unit === "M" ? "CM" || exercise.unit === "CM" : "M"}:{" "}
+              {exercise.unit === "M"
+                ? answer * 100 + "CM" || exercise.unit === "CM"
+                : (answer * 0.01).toFixed(3) + "M"}
+              <sup>2</sup>
+            </span>
+          ) : (
+            <span className="geometric_result">
+              Answer in{" "}
+              {exercise.unit === "M" ? "CM" || exercise.unit === "CM" : "M"}:{" "}
+              {exercise.unit === "M"
+                ? answer * 100 + "CM" || exercise.unit === "CM"
+                : (answer * 0.01).toFixed(2) + "M"}{" "}
+            </span>
+          )}
         </div>
       </div>
     </div>
--- src/components/GeometricFigures/Square/Square.jsx
+++ src/components/GeometricFigures/Square/Square.jsx
@@ -1,9 +1,142 @@
-import React from 'react'
-
+import React, { useState } from "react";
+//import "./rectangle.css";
+const data = {
+  length: 0,
+  unit: "",
+  dimension: "",
+};
 const Square = () => {
-  return (
-    <div>Square</div>
-  )
-}
+  const [exercise, setExercise] = useState(data);
+  const [answer, setAnswer] = useState(0);
+  const [error, setError] = useState("");
+  const [unit, setUnit] = useState("");
 
-export default Square
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target;
+
+    setExercise((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (exercise.dimension === "") {
+      setError("Please choose the dimension you want to calculate");
+    }
+    e.preventDefault();
+    if (exercise.unit === "") {
+      setError("Please select the unit for calculation");
+    } else {
+      console.log(exercise);
+      if (exercise.dimension === "Perimeter") {
+        setAnswer((Number(exercise.length) * 4 ));
+      } else if (exercise.dimension === "Area") {
+        setAnswer(
+          Number(exercise.length) * Number(exercise.length),
+          exercise.unit
+        );
+        setUnit(exercise.unit);
+      }
+    }
+    e.target.reset();
+  };
+
+  return (
+    <div className="app__figure_container">
+      <div className="app__figure__header">
+        <h3>Find the Area and Perimeter of A Square</h3>
+      </div>
+      <div className="app__form">
+        <form onSubmit={submitHandler}>
+          <div className="form-group ">
+            <div className="sub_form-group">
+              <input
+                type="radio"
+                id="area"
+                name="dimension"
+                value="Area"
+                onChange={onChangeHandler}
+              />
+              <label htmlFor="area">Area</label>
+            </div>
+            <span className="error">{error}</span>
+          </div>
+          <div className="form-group ">
+            <div className="sub_form-group">
+              <input
+                type="radio"
+                id="area"
+                name="dimension"
+                value="Perimeter"
+                onChange={onChangeHandler}
+              />
+              <label htmlFor="perimeter">Perimeter</label>
+            </div>
+            <span className="error">{error}</span>
+          </div>
+          <div className="form-group">
+            <input
+              type="number"
+              name="length"
+              onChange={onChangeHandler}
+              placeholder="Enter Length"
+            />
+            <span className="error">{error}</span>
+          </div>
+          <div className="form-group">
+            <select name="unit" onChange={onChangeHandler}>
+              <option>
+                Select unit (e.g M for Meters or CM for Centimeters)
+              </option>
+              <option value="M">M</option>
+              <option value="CM">CM</option>
+            </select>
+            <span className="error">{error}</span>
+          </div>
+          <div className="form-group">
+            <button className="btn__custom" type="submit">
+              {exercise.dimension === "Perimeter"
+                ? " Calculate " + exercise.dimension
+                : exercise.dimension === "Area"
+                ? " Calculate " + exercise.dimension
+                : "Calculate"}
+            </button>
+          </div>
+        </form>
+        <div className="app__result_container">
+          {exercise.dimension === "Area" ? (
+            <span className="geometric_result">
+              Answer in {exercise.unit} : {answer} {exercise.unit}
+              <sup>2</sup>
+            </span>
+          ) : (
+            <span className="geometric_result">
+              Answer in {exercise.unit}: {answer} {exercise.unit}
+            </span>
+          )}
+          {exercise.dimension === "Area" ? (
+            <span className="geometric_result">
+              Answer in{" "}
+              {exercise.unit === "M" ? "CM" || exercise.unit === "CM" : "M"}:{" "}
+              {exercise.unit === "M"
+                ? answer * 100 + "CM" || exercise.unit === "CM"
+                : (answer * 0.01).toFixed(3) + "M"}
+              <sup>2</sup>
+            </span>
+          ) : (
+            <span className="geometric_result">
+              Answer in{" "}
+              {exercise.unit === "M" ? "CM" || exercise.unit === "CM" : "M"}:{" "}
+              {exercise.unit === "M"
+                ? answer * 100 + "CM" || exercise.unit === "CM"
+                : (answer * 0.01).toFixed(2) + "M"}{" "}
+            </span>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Square;
